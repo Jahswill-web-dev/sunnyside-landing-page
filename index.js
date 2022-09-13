ee28fdf83a51ad64a879cf4d80696b629c9f4a30
Jahswill <76982529+Jahswill-web-dev@users.noreply.github.com>
2022-09-13 15:10:26 +0100

    fixed some bugs
    fixed the bug with navigation.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const hamburgerBtn = document.querySelector('div.hamburger');
-const mobileNav = document.querySelector('div.mobile-menu')  
+const mobileNav = document.querySelector('div.mobile-menu');  
 
-hamburgerBtn.addEventListener('click', function(){
-    mobileNav.classList.toggle('show')
-    mobileNav.classList.toggle('mobile-menu')
+hamburgerBtn.addEventListener('click', () =>{
+    mobileNav.classList.toggle('show');
+    mobileNav.classList.toggle('mobile-menu');
     
-})
+});
